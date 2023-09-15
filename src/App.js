b4e040a5add052a6413f65f07515de54a1791edc
LuisEduardo821 <lerivadeneira@pucesi.edu.ec>
2023-09-15 17:27:40 -0500

    segundo commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,51 @@
 import "./App.css";
+import logoFreeCodeCamp from "./imagenes/FreeCodeCamp_logo.png";
+import Boton from "./componentes/Boton";
+import Pantalla from "./componentes/Pantalla";
 
 function App() {
-  return <div className="App">Hola</div>;
+  return (
+    <div className="App">
+      <div className="contenedor-logo">
+        <img
+          src={logoFreeCodeCamp}
+          alt="logo-freeCodeCamp"
+          className="freecodecamp-logo"
+        />
+      </div>
+      <div className="contenedor-calculadora">
+        <Pantalla />
+
+        <div className="fila">
+          <Boton>1</Boton>
+          <Boton>2</Boton>
+          <Boton>3</Boton>
+          <Boton>+</Boton>
+        </div>
+        <div className="fila">
+          <Boton>4</Boton>
+          <Boton>5</Boton>
+          <Boton>6</Boton>
+          <Boton>-</Boton>
+        </div>
+        <div className="fila">
+          <Boton>7</Boton>
+          <Boton>8</Boton>
+          <Boton>9</Boton>
+          <Boton>*</Boton>
+        </div>
+        <div className="fila">
+          <Boton>=</Boton>
+          <Boton>0</Boton>
+          <Boton>.</Boton>
+          <Boton>/</Boton>
+        </div>
+        <div className="fila">
+          <Boton>Borrar</Boton>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/componentes/Pantalla.js
@@ -0,0 +1,5 @@
+import React from "react";
+import "../hoja-estilos/Pantalla.css";
+
+const Pantalla = ({ input }) => <div className="input">{input}</div>;
+export default Pantalla;
